52f0cda6dc3cacb3cb9ee0c298c2d2a680f6c8e8
mzakharow <zaharow@bk.ru>
2018-08-08 00:48:40 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import requests
+
+TOKEN = '687525307:AAETuwa-twszYp_HUw9hrySbUIMoKBRkiI0'
+MAIN_URL = f'https://api.telegram.org/bot{TOKEN}'
+
+# proxies = {
+#     'http': 'http://10.10.1.10:3128',
+#     'https': 'https://10.10.1.10:1080',
+# }
+#
+# r = requests.get(f'{MAIN_URL}/getUpdates', proxies=proxies)
+
+# # ответ
+# payload = {
+#     'char_id': 1111,
+#     'text': 'Hello',
+#     'reply_to_message_id': 1
+# }
+#
+# r = requests.post(f'{MAIN_URL}/sendMessage', data=payload)
+
+# получение данных
+r = requests.get(f'{MAIN_URL}/getUpdates')
+
+print(r.json())
